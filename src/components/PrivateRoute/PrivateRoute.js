52184879fa076ebf809c7e5712e48710e416bded
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-09 09:16:51 +0600

    7

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from "react-router";
 
 const PrivateRoute = ({ children, ...rest }) => {
-  //   const { children, ...rest } = props;
+  //  const { children, ...rest } = props; 
   return <Route {...rest}></Route>;
 };
 
