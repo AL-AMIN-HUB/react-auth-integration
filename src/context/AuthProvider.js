bd8f51f283294884cfbbc450deeed90184c64673
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-09 08:20:41 +0600

    5

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createContext } from "react";
+import useFirebase from "../hooks/useFirebase";
+const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  //   const { children } = props;
+  const allContext = useFirebase();
+  return <AuthContext.Provider value={allContext}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
